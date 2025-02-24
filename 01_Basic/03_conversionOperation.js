049f2bf990379b6ed822022aacf811ba9b8a7300
daystaraliya <114356339+daystaraliya@users.noreply.github.com>
2025-02-24 17:19:20 +0000

    conversion of datatype

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 43
+let no = "33"
+console.log(typeof score);//number
+console.log(typeof no);//string
+//conversion
+let valueNo = Number(no)
+console.log(typeof valueNo);
+console.log(valueNo)
+
+let a = undefined
+let b = Number(a)
+console.log(a);
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+let c = String(score)
+console.log(typeof c)
+
+
+
+
+
--- README.md
+++ README.md
@@ -10,5 +10,9 @@
 5. null : standalone value/it's representation of empty value
 6. undefined : non assigned variable/when you have not defined value of declared variable
 7. symbol- uniqueness &immutable,used oftenly as a key of abject
+#conversion
+into "Number" - Number(variable)
+"33" -> 33
+"true" -> 1, "false"->0
 
 
