a530627eadc2a721ba35766be8a3f22e112e2f2f
Aliya Tabassum <114356339+daystaraliya@users.noreply.github.com>
2025-07-14 18:26:36 +0000

    some more conversion of data types added

--- 01_Basic/02_dataType.js
+++ 01_Basic/02_dataType.js
@@ -1,11 +1,37 @@
 "use Strict";//treating js code as new version
+
  age = 32
  name = "Aliya"
  let marks = 89.83
- //console.table([typeof age , name , marks])
+//  console.table([typeof age , name , marks])
 console.log(typeof age);
 console.log(typeof name);
 console.log(typeof marks);
 console.log(typeof null);
 console.log(typeof undefined);
+
+ let no = "22abc";
+ let score = Number(no);
+ console.log(typeof score);
+ console.log(score);
+ 
+ let isLoggedIn = 1;
+ let booleanLoggedIn = Boolean(isLoggedIn);
+
+ console.log(typeof booleanLoggedIn);
+ //1 = true , 0 = false
+ let str = "";
+
+ let BooleanStr = Boolean(str);
+ console.log(BooleanStr);
+ //"" = false , "any value"= true
+ 
+ let x = 10;
+ let StrX = String(x);
+ console.log(StrX);
+ console.log(typeof strX);
+ 
+ 
+ 
+ 
 
